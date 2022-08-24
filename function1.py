944b90dcc9f2483d3a5055f3c9e59aec6066cae1
Marisol Rodriguez <maridrodriguez@gmail.com>
2022-08-24 12:12:41 -0400

    max_num function to find maximum of 3 numbers

---
+++ function1.py
@@ -0,0 +1,7 @@
+# Activity: Python Function Practice Part 4
+# Python Function called max_num()to find the maximum of three numbers
+def max_num(a,b,c):
+    return max([a,b,c])
+print(max_num(4,5,6))
+print(max_num(500,700,800))
+print(max_num(18,22,21))
