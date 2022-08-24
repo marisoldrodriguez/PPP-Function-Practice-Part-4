230964d9d219f3787b4fdf7101340bb3d30ec9b7
Marisol Rodriguez <maridrodriguez@gmail.com>
2022-08-24 13:24:36 -0400

    reverse string function

---
+++ function2.py
@@ -0,0 +1,13 @@
+# Python function called mult_list() to multiply all the numbers in a list
+def mult_list(list):
+    if len(list) == 0:
+        return 0
+    prod = list[0]
+    if len(list) > 1:
+        for i in list[1:]:
+            prod = prod * i 
+    return prod
+print(mult_list([10]))
+print(mult_list([2,4,6]))
+print(mult_list([]))
+    
---
+++ function3.py
@@ -0,0 +1,7 @@
+# Python function called rev_string() to reverse a string
+def rev_string(my_string):
+    return my_string[::-1]
+
+print(rev_string(""))
+print(rev_string("apple"))
+print(rev_string("Marisol"))
