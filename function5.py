0a0d0ff73fffcb9f95fb3a54f671db283f32dbd8
Marisol Rodriguez <maridrodriguez@gmail.com>
2022-08-24 14:24:04 -0400

    function for Pascal's triangle

---
+++ function5.py
@@ -0,0 +1,48 @@
+# Python function called pascal() that prints out the first n rows of Pascal's triangle
+triangle = [[1],[1,1]]
+def pascal(n):    
+	#base case
+    if n < 1:
+        print("invalid number of rows")
+    elif n == 1:
+        print(triangle[0])
+    else:
+        row_number = 2
+        #fill up correct number of rows in triangle
+        while len(triangle) < n:
+            row = []
+            row_prev = triangle[row_number - 1]
+            #create correct row, then add to triangle (this row will be 1 longer than row before it)
+            length = len(row_prev)+1
+            for i in range(length):
+                #first number is 1
+                if i == 0:
+                    row.append(1)
+                #intermediate nunmbers get added from previous rows
+                elif i > 0 and i < length-1:
+                    row.append(triangle[row_number-1][i-1]+triangle[row_number-1][i])
+                #last number is 1
+                else:
+                    row.append(1)
+            triangle.append(row)
+            row_number += 1
+
+        #print triangle
+        for row in triangle:
+            print(row)
+
+pascal(1)
+'''
+output:
+1
+'''
+
+pascal(5)
+'''
+output:
+1
+1 1
+1 2 1
+1 3 3 1
+1 4 6 4 1
+'''                                                              
